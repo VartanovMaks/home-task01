d565532f0b7b9dcb6292b292c00b050fbc3d7b00
VartanovMaks <vartanov2009@gmail.com>
2021-03-26 12:02:26 +0200

    Кнопки сброс и смена направления удаления

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import React, {Component, useState} from 'react';
 // (або останній)  якщо у нас масив з 3 елементів і ми 	клікнули на кнопку
 //  3 рази, то на екрані жодна карточка не має відмалюватись  (кнопки повернення 
 // 	до початкового стану не треба)
+// 2 - те саме, тільки з кнопкою реверт (повернутись до стану, де у нас видно 3 елемнети,
+// як на початку)
+// Плюс я додав кнопку зміни напрамку
 
 let posters =[
-  {id:1, show:true, img:'img/213px-Looper_xlg.jpg', name:'Петля времени', year:2012},
-  {id:2, show:true, img:'img/225px-Die_Hard.gif', name:'Крепкий орешек', year:1988},
-  {id:3, show:true, img:'img/Девять_ярдов_(постер).jpg', name:'Девять ярдов', year:2000},
-  {id:4, show:true, img:"img/216px-Armageddon-poster.jpg", name:'Армагеддон', year:1998},
+  {id:1, img:'img/213px-Looper_xlg.jpg', name:'Петля времени', year:2012},
+  {id:2, img:'img/225px-Die_Hard.gif', name:'Крепкий орешек', year:1988},
+  {id:3, img:'img/Девять_ярдов_(постер).jpg', name:'Девять ярдов', year:2000},
+  {id:4, img:"img/216px-Armageddon-poster.jpg", name:'Армагеддон', year:1998},
   ]
 
 const Poster = ({poster}) => (
@@ -19,58 +22,57 @@ const Poster = ({poster}) => (
     <h3>{poster.name}.</h3>
     <p>{poster.year} год.</p>
     <img src={poster.img} alt={poster.name}></img>
-
   </div>
-
-
 )
 
 function App() {
-
-  //posters.forEach(item=>item.show=true);
   
-  // const [cards,setState] = useState(posters)
-  // let l = posters.length
-  // const [counter,setCounter] = useState(l)
-  let l = posters.length
+  // У стейті зберігаємо весь масив, та змінну напрямку видалення
   const [state,setState] = useState({
-    counter:l,
-    p:[...posters]
+    p:[...posters],
+    direction:true
+    //true с конца
   })
-  
-
-  const clickHandler = ()=>{
-    // уменьшаем счетчик нажатий на кнопку
-    // соответствующему элементу массива присваиваем св-во show - false
-    
-    if(state.counter > 0) {
-      let cnt=state.counter-1;
-      let newArr = [...state.p]
-      newArr[cnt].show=false;
+  const clickReset= ()=>{
+    // заганяємо у стейт масив заново
+    setState({
+        ...state,
+        p:[...posters]
+      })
+    }
+    const clickChangeDirection = () =>{
       setState({
-        //...state,
-        counter:cnt,
+        ...state,
+        direction:!state.direction
+      })
+    }
+    
+    const clickHandler = ()=>{
+      let newArr=[];
+      if (state.direction) {
+        //true видаляэмо останній елемент
+        newArr = state.p.slice(0,state.p.length-1)
+      } else{
+          //false видаляэмо перший елемент
+        newArr = state.p.slice(1,state.p.length)
+      }
+      setState({
+        ...state,
         p:newArr
       })
-      
-      console.log(state.counter)
-      console.log(state.p)
-    }
 }
 
-
   return (
-
-
-
     <div className="App">
-      <button onClick={clickHandler}> Убрать последний элемент </button>
+      <button onClick={clickReset}> Reset </button>
+      {state.direction && <button onClick={clickChangeDirection}> Напрям з кінця </button>}
+      {!state.direction && <button onClick={clickChangeDirection}> Напрям з початку </button>}
+      {state.direction && <button onClick={clickHandler}> Видалити останній елемент </button>}
+      {!state.direction && <button onClick={clickHandler}> Видалити перший елемент </button>}
       <div className="cards">
         {
-          state.p.map( poster=>( 
-            //{state.isHeaderVisible && <Header counter = {state.counter}/>}
-            poster.show && <Poster key={poster.id} poster={poster}/>
-          ))
+          state.p.map( poster=>
+            <Poster key={poster.id} poster={poster}/>)
         }
       </div>
     </div>
