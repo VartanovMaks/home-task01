42f9b5cd0013acdac633a839dcbd156314d505bd
VartanovMaks <vartanov2009@gmail.com>
2021-03-29 10:23:39 +0300

    Первый комит - передаем ид кнопки

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import React, {Component, useState} from 'react';
 
-// відмалювати список карточок базуючись на якомусь створеному вами масиві 
-// створити окрему кнопку, 	яка буде видаляти поточний перший елемент 
-// (або останній)  якщо у нас масив з 3 елементів і ми 	клікнули на кнопку
-//  3 рази, то на екрані жодна карточка не має відмалюватись  (кнопки повернення 
-// 	до початкового стану не треба)
-// 2 - те саме, тільки з кнопкою реверт (повернутись до стану, де у нас видно 3 елемнети,
-// як на початку)
-// Плюс я додав кнопку зміни напрамку
+// 3 задача з зірочкою) кожна карточка з завдання вище має мати кнопку, 
+// по кліку на яку, ми видаляємо зі списку саме її + реверт кнопка, щоб 
+// вернути все назад (ця кнопка одна дня всіх карточок, клікнули 
+// по ній і всі каркти вернулись назазд) (згадування функції фільтр в лекції 
+// було не просто так)
 
 let posters =[
   {id:1, img:'img/213px-Looper_xlg.jpg', name:'Петля времени', year:2012},
@@ -17,21 +14,26 @@ let posters =[
   {id:4, img:"img/216px-Armageddon-poster.jpg", name:'Армагеддон', year:1998},
   ]
 
+function App() {
+
+const PosterButton = ({d}) => (
+    
+    <button onClick={clickPosterButton} id={d}> Видалити постер </button>
+)
 const Poster = ({poster}) => (
   <div className='poster-card'>
     <h3>{poster.name}.</h3>
     <p>{poster.year} год.</p>
-    <img src={poster.img} alt={poster.name}></img>
+    <img src={poster.img} alt={poster.name} id={poster.id}></img>
+    <PosterButton d={poster.id}/>
   </div>
 )
-
-function App() {
-  
+  const clickPosterButton = (e)=>{
+  console.log(e);
+}
   // У стейті зберігаємо весь масив, та змінну напрямку видалення
   const [state,setState] = useState({
     p:[...posters],
-    direction:true
-    //true с конца
   })
   const clickReset= ()=>{
     // заганяємо у стейт масив заново
@@ -40,35 +42,9 @@ function App() {
         p:[...posters]
       })
     }
-    const clickChangeDirection = () =>{
-      setState({
-        ...state,
-        direction:!state.direction
-      })
-    }
-    
-    const clickHandler = ()=>{
-      let newArr=[];
-      if (state.direction) {
-        //true видаляэмо останній елемент
-        newArr = state.p.slice(0,state.p.length-1)
-      } else{
-          //false видаляэмо перший елемент
-        newArr = state.p.slice(1,state.p.length)
-      }
-      setState({
-        ...state,
-        p:newArr
-      })
-}
-
   return (
     <div className="App">
       <button onClick={clickReset}> Reset </button>
-      {state.direction && <button onClick={clickChangeDirection}> Напрям з кінця </button>}
-      {!state.direction && <button onClick={clickChangeDirection}> Напрям з початку </button>}
-      {state.direction && <button onClick={clickHandler}> Видалити останній елемент </button>}
-      {!state.direction && <button onClick={clickHandler}> Видалити перший елемент </button>}
       <div className="cards">
         {
           state.p.map( poster=>
