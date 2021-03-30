1af25896ca11fd6d58fa0f1dcde4571eba0931f0
VartanovMaks <vartanov2009@gmail.com>
2021-03-30 16:03:12 +0300

    сделали массив табов

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 
-const Btn = (title)=>{
+const tabs=[{name:'Posts', url:'https://jsonplaceholder.typicode.com/posts'},
+            {name:'Albums', url:'https://jsonplaceholder.typicode.com/albums'},
+            {name:'Photos', url:'https://jsonplaceholder.typicode.com/photos'},
+            {name:'Todos', url:'https://jsonplaceholder.typicode.com/todos'},
+            {name:'Users', url:'https://jsonplaceholder.typicode.com/users'},
+            {name:'Comments', url:'https://jsonplaceholder.typicode.com/comments'}
+          ]
+
+const Btn = ({title})=>{
+
+  const clickBtn = () =>{
+
+  }
 
   return(
-    <button>{title}</button>
+    <button onClick={clickBtn}>{title}</button>
   )
 }
-
-
 
 
 const App = () =>{
 
 return (
   <>
-    <Btn title='Posts' />
-    <Btn title='Comments' />
-    <Btn title='Albums' />
-    <Btn title='Photos' />
-    <Btn title='Todos' />
-    <Btn title='Users' />
+    <div className='btn-div'>
+      {tabs.map(tab => <Btn title={tab.name} />)}
+    </div>
+    <hr />
+    <div className='main-div'></div>
   </>
-
 )
 
 }
