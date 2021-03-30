3cd32e55f93ce80c4abad9ed73e2c3195f029413
VartanovMaks <vartanov2009@gmail.com>
2021-03-30 14:50:04 +0300

    Начало

--- src/App.js
+++ src/App.js
@@ -1,56 +1,32 @@
 import './App.css';
 import React, {Component, useState} from 'react';
 
-// 3 задача з зірочкою) кожна карточка з завдання вище має мати кнопку, 
-// по кліку на яку, ми видаляємо зі списку саме її + реверт кнопка, щоб 
-// вернути все назад (ця кнопка одна дня всіх карточок, клікнули 
-// по ній і всі каркти вернулись назазд) (згадування функції фільтр в лекції 
-// було не просто так)
+const Btn = (title)=>{
 
-let posters =[
-  {id:1, img:'img/213px-Looper_xlg.jpg', name:'Петля времени', year:2012},
-  {id:2, img:'img/225px-Die_Hard.gif', name:'Крепкий орешек', year:1988},
-  {id:3, img:'img/Девять_ярдов_(постер).jpg', name:'Девять ярдов', year:2000},
-  {id:4, img:"img/216px-Armageddon-poster.jpg", name:'Армагеддон', year:1998},
-  ]
-
-function App() {
-
-const PosterButton = ({d}) => (
-    // кнопка отримує id таке саме як і постер id
-    <button onClick={clickPosterButton} id={d}> Видалити постер </button>
-)
-const Poster = ({poster}) => (
-  <div className='poster-card'>
-    <h3>{poster.name}.</h3>
-    <p>{poster.year} год.</p>
-    <img src={poster.img} alt={poster.name} id={poster.id}></img>
-    <PosterButton d={poster.id}/>
-  </div>
-)
-  const clickPosterButton = (e)=>{
-  let newPstrs = state.filter( p => p.id.toString() !== e.target.id)
-  setState([...newPstrs])
+  return(
+    <button>{title}</button>
+  )
 }
-  // У стейті зберігаємо весь масив, та змінну напрямку видалення
-  const [state,setState] = useState([...posters])
-  
-  const clickReset= ()=>{
-    // заганяємо у стейт масив заново
-    setState([...posters])
-    }
 
-  return (
-    <div className="App">
-      <button onClick={clickReset}> Reset </button>
-      <div className="cards">
-        {
-          state.map( poster=>
-            <Poster key={poster.id} poster={poster}/>)
-        }
-      </div>
-    </div>
-  );
+
+
+
+const App = () =>{
+
+return (
+  <>
+    <Btn title='Posts' />
+    <Btn title='Comments' />
+    <Btn title='Albums' />
+    <Btn title='Photos' />
+    <Btn title='Todos' />
+    <Btn title='Users' />
+  </>
+
+)
+
 }
+
+
 
 export default App;
