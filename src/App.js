8352bfb21e0b53db56a35c62c289e85bea668e1d
VartanovMaks <vartanov2009@gmail.com>
2021-03-31 13:07:07 +0300

    Общий механизм работает

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import './App.css';
 import React, {Component, useState, useEffect} from 'react';
 
-const tabs=[{name:'Posts', url:'https://jsonplaceholder.typicode.com/posts'},
-            {name:'Albums', url:'https://jsonplaceholder.typicode.com/albums'},
-            {name:'Photos', url:'https://jsonplaceholder.typicode.com/photos'},
-            {name:'Todos', url:'https://jsonplaceholder.typicode.com/todos'},
-            {name:'Users', url:'https://jsonplaceholder.typicode.com/users'},
-            {name:'Comments', url:'https://jsonplaceholder.typicode.com/comments'}
+const tabs=[{id:0,name:'Posts', url:'https://jsonplaceholder.typicode.com/posts'},
+            {id:1,name:'Photos', url:'https://jsonplaceholder.typicode.com/photos'},
+            {id:2,name:'Albums', url:'https://jsonplaceholder.typicode.com/albums'},
+            {id:3,name:'Todos', url:'https://jsonplaceholder.typicode.com/todos'},
+            {id:4,name:'Users', url:'https://jsonplaceholder.typicode.com/users'},
+            {id:5,name:'Comments', url:'https://jsonplaceholder.typicode.com/comments'}
           ]
 
-const Btn = ({title})=>{
+const App = () =>{
+ 
+ const [tabList, setTabList] = useState([])
+ const [tabSelected, setTabSelected] = useState(tabs[0])
 
-  const clickBtn = () =>{
-
+  const fetchLoad = async()=>{
+    const response = await fetch(tabSelected.url);
+    const data = await response.json();
+    console.log(data)
+    setTabList(data)
+    // console.log(tabList)
   }
 
-  return(
-    <button onClick={clickBtn}>{title}</button>
+  // Mount & Unmount
+  useEffect( ()=>{
+    fetchLoad();
+    return ()=> setTabList(null)
+  },[])
+
+  // Update
+  useEffect( ()=>{
+    fetchLoad();
+  },[tabSelected])
+  
+  const clickBtn = ({tab}) =>{
+    let newTab = JSON.parse(JSON.stringify(tab))
+    console.log(newTab)
+    setTabSelected(newTab)
+  }
+
+  return (
+    <>
+      <div className='btn-div'>
+        {tabs.map(tab => <button onClick={()=> clickBtn({tab})}id={tab.id} key={tab.name}>{tab.name}</button>)}
+      </div>
+      <hr />
+      <div className='main-div'>
+        <ul>
+        {tabList.map(item => <li key={item.id}>{tabSelected.name} {item.length} : {item.id}</li>)}
+        </ul>
+      </div>
+    </>
   )
-}
-
-
-const App = () =>{
-
-return (
-  <>
-    <div className='btn-div'>
-      {tabs.map(tab => <Btn title={tab.name} />)}
-    </div>
-    <hr />
-    <div className='main-div'></div>
-  </>
-)
 
 }
-
-
 
 export default App;
